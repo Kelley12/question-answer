bb69956beb3130842493190689b592e6935f9fff
Blake <blakekelley127@gmail.com>
2019-12-08 09:24:54 -0700

    Adding usage to the landing page

--- src/routers/question.js
+++ src/routers/question.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = new express.Router()
 
+router.get('/', (req, res) => {
+    const header = "<h1>Web Service Usage:</h1>";
+    const usage = "<p>Enter a question using the query string parameter 'q'.</p>";
+    const example = `<p><b>Example:</b>`;
+    const link = `<a href='${req.headers.host}/question?q=question text'>${req.headers.host}/question?q=question text</a></p>`;
+
+    res.send(`${header} ${usage} ${example} ${link}`);
+});
+
 // GET /question?q=plain text question
 router.get('/question', (req, res) => {
     try {
